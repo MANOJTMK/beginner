1b7bd340d49e3a74f7fa23551cf00b8b20fdff1b
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-09 11:03:44 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+    int fact=1;
+    int i;
+    int n;
+    scanf("%d",&n);
+    int sum;
+    for(i=1;i<6;i++)
+    {   sum=n*i;
+        printf("%d*%d=%d",n,i,sum);
+        printf("\n");
+    }
+	return 0;
+}
